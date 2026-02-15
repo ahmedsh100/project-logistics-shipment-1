72988e64c68bf5d35068e3afd32ed6b62d119811
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2026-02-15 03:23:31 +0200

    Update Views..

---
+++ resources/views/them/master.blade.php
@@ -0,0 +1,22 @@
+
+@include("partial.head")
+
+<body class="contact-page">
+
+@include("partial.hero")
+
+    <!-- Contact Section -->
+    @yield("content")
+
+ 
+
+    @include("partial.footer")
+
+
+  <!-- Vendor JS Files -->
+    @include("partial.script")
+
+
+</body>
+
+</html>
